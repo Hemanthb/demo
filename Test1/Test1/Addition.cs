abd54497b85fb6703a5223e9bec325eff480b7ab
hem_b <hemanthb93@gmail.com>
2022-05-13 12:09:02 +0530

    [hemanth]: addition of 2 nums

---
+++ Test1/Test1/Addition.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Addition
+    {
+        public int add(int a,int b)
+        {
+            int result = a + b;
+            return result;
+        }
+    }
+}
---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Test1
+    // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.WriteLine("Enter first number");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Addition addition = new Addition();
+            z = addition.add(x, y);
+            Console.WriteLine("Addition of 2 number is" + z);
+        }
+    }
+}
